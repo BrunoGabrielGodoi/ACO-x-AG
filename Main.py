f317bc58d97d880fc6a0d9279696715a33ff4710
brulufa9 <bruno_godoi@hotmail.com.br>
2019-04-23 20:28:06 -0300

    Graph Added

--- Main.py
+++ Main.py
@@ -1,19 +1,45 @@
+import random
+
+
 
 class Individual:
 
     def __init__(self, path):
         self.path = path
 
+
 class Graph:
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, size, arestas):
+        self.size = size
+        self.arestas = arestas
+
+
+def GeneratePopulation(sizeOfPopulation):
+
+    path = []
+
+    for i in range(0,sizeOfPopulation):
+        path.append(1)
+
+        temp = random.randint(1, 5) # 0,5 = 1-5
+        
 
 
 def Main():
-    graph = ["A","B","C","D","E","F"]
-    i1 = Individual(graph)
+
+    graphSize = 5
+    graphA = [  [X,1,2,3,4,5],
+                [1,0,2,0,3,6],
+                [2,2,0,4,3,0],
+                [3,0,4,0,7,3],
+                [4,3,3,7,0,3],
+                [5,6,0,3,3,0]]
+
+    graph = Graph(graphSize,graphA)
+
     print(i1.path[1])   
+    population = GeneratePopulation()
 
 
 
