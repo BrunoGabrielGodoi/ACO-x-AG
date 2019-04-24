9dbadfcf1b06d7773169f99dec3502c3615e7f13
brulufa9 <bruno_godoi@hotmail.com.br>
2019-04-23 21:30:15 -0300

    Generation Implementada

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 class Individual:
 
     def __init__(self, path):
@@ -14,22 +12,61 @@ class Graph:
         self.size = size
         self.arestas = arestas
 
+    def IsConnected(self,a,b):
+        if self.arestas[a][b] != 0:
+            return True
+        else: 
+            return False
 
-def GeneratePopulation(sizeOfPopulation):
 
-    path = []
+def GeneratePopulation(sizeOfPopulation, graph):
+  
+    path = [1]
+    population = []  
 
     for i in range(0,sizeOfPopulation):
+        j = 1
+        while j < (graph.size + 1):
+            
+            compatible = False
+            ErrorCounter = 0
+            while compatible == False:
+
+                temp = random.randint(1, 5) # 0,5 = 1-5
+                a = path[-1]
+                ErrorCounter += 1
+                if graph.IsConnected(a,temp):
+
+                    if temp not in path :
+                        path.append(temp)
+                        compatible = True
+                    elif len(path) == 5:
+                        path.append(1)
+                        compatible = True
+
+                elif ErrorCounter > (graph.size*4): # Tester no futuro se isso está funcionando mesmo
+
+                    path.clear()
+                    path.append(1)
+                    j = 1
+                    ErrorCounter = 0
+            j += 1
+        population.append(Individual(path))
+        print(i,"-",path)
+        path.clear()
         path.append(1)
 
-        temp = random.randint(1, 5) # 0,5 = 1-5
-        
+    return population
+
+
+
+
 
 
 def Main():
 
     graphSize = 5
-    graphA = [  [X,1,2,3,4,5],
+    graphA = [  [0,1,2,3,4,5],
                 [1,0,2,0,3,6],
                 [2,2,0,4,3,0],
                 [3,0,4,0,7,3],
@@ -38,9 +75,10 @@ def Main():
 
     graph = Graph(graphSize,graphA)
 
-    print(i1.path[1])   
-    population = GeneratePopulation()
-
+       
+    population = GeneratePopulation(1000,graph)
+    
+   # print(population)
 
 
 
